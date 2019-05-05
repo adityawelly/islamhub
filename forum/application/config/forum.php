dbbd3f2e4ee7fd01e8a4bb4de0cdc796bf839000
root <alfigusman0@gmail.com>
2019-05-05 21:30:56 +0700

    Initial commit

---
+++ forum/application/config/forum.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Forum config
+|--------------------------------------------------------------------------
+|
+| This determines the basic forum config
+|
+*/
+$config['site_title'] = 'Forum';
+$config['site_slogan'] = 'This is my forum';
+$config['site_email'] = 'alfi.gusman.9f@gmail.com';
