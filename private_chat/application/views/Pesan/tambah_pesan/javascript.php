b8a5cd5a4ba48a3e04397182c0cc35fbc7e9a6ed
adityawelly <wellyadit@gmail.com>
2019-05-14 18:37:15 +0700

    ngehapus

--- private_chat/application/views/Pesan/tambah_pesan/javascript.php
+++
@@ -1,5 +0,0 @@
-<script src="<?=base_url('assets/plugins/ckeditor/styles.js')?>"></script>
-<script src="<?=base_url('assets/plugins/ckeditor/ckeditor.js')?>"></script>
-<script>
-    CKEDITOR.replace( 'isiPesan' );
-</script>
