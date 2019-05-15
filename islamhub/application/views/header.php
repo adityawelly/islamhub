1da827225cbabc5b6ed7bc224f9f703763494f5b
wildannajahw <wildan.najah@gmail.com>
2019-05-15 09:38:22 +0700

    nambah pencarian video dan buku LAGI

--- islamhub/application/views/header.php
+++ islamhub/application/views/header.php
@@ -12,13 +12,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   
-	<link href="<? base_url('assets/css/style.css') ?>" rel="stylesheet">
+	
+    <link href="http://localhost/islamhub/islamhub/assets/css/kontent.css" rel="stylesheet" />
+
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-md navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="http://localhost/islamhub/kontent/video/">IslamHub</a>
+                <a class="navbar-brand" href="http://localhost/islamhub/islamhub/">IslamHub</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -28,11 +30,11 @@
                             <div class="d-flex justify-content-center h-100">
                                 <div class="searchbar">
                                     <input class="search_input" type="text" name="" placeholder="Search...">
-                                    <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+                                    <a href="pencarian" class="search_icon"><i class="fas fa-search"></i></a>
                                 </div>
                             </div>
                         </div>
-                        <a class="nav-item btn tombol" href="<?=base_url()?>video">Videos</a>
+                        <a class="nav-item btn tombol" href="<?=base_url()?>video/">Videos</a>
                         <a class="nav-item btn tombol1" href="<?=base_url()?>buku/">Books</a>
                         <a class="nav-item btn tombol1" href="<?=base_url()?>Dashboard/">Private Consult</a>
                         <a class="nav-item btn tombol1" href="<?=base_url()?>Forum/">Forum</a>
