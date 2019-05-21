0992afad8c0da0ad9d8347008ff61b8f19776286
alwanyassin <alwanyassin@gmail.com>
2019-05-21 14:57:50 +0700

    update tampilan depan

--- islamhub/application/views/footer.php
+++ islamhub/application/views/footer.php
@@ -1,12 +1,82 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-</main><!-- #site-content -->
+<footer class="footer primary-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                    	<div class="widget clearfix">
+                    		<h4 class="widget-title">Subscribe</h4>
+                    		<div class="newsletter-widget">
+                    			<p>You can opt out of our newsletters at any time. See our privacy policy.</p>
+		                        <form class="form-inline" role="search">
+		                            <div class="form-1">
+		                              	<input type="text" class="form-control" placeholder="Enter email here..">
+										<button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i></button>
+		                            </div>
+								</form>
+                    		</div><!-- end newsletter -->
+                    	</div><!-- end widget -->
+                    </div><!-- end col -->
+                    <div class="col-md-3 col-sm-3">
+                    	<div class="widget clearfix">
+                    		<h4 class="widget-title">IslamHub</h4>
+                    		<ul>
+                    			<li><a href="#">About us</a></li>
+                    			<li><a href="#">Carrier</a></li>
+                    			<li><a href="#">Contact</a></li>
+                    		</ul>
+                    	</div><!-- end widget -->
+                    </div><!-- end col -->
 
-	<footer id="site-footer" role="contentinfo">
-	</footer><!-- #site-footer -->
+                    <div class="col-md-3 col-sm-3">
+                    	<div class="widget clearfix">
+                    		<h4 class="widget-title">Lainnya</h4>
+                    		<ul>
+                    			<li><a href="#">Syarat & Ketentuan</a></li>
+                    			<li><a href="#">Privasi</a></li>
+                    			<li><a href="#">Daftar Pakar</a></li>
+                    		</ul>
+                    	</div><!-- end widget -->
+                    </div><!-- end col -->
 
-	<!-- js -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="<? echo "base_url('assets/js/script.js')" ?>"></script>
+                    <div class="col-md-2 col-sm-2">
+                    	<div class="widget clearfix">
+                    		<h4 class="widget-title">Be Social</h4>
+                    		<ul>
+								<li><a href="www.instagram.com/adityawelly">Instagram</a></li>
+                    			<li><a href="#">Facebook</a></li>
+                    			<li><a href="www.twitter.com/adityawellyy">Twitter</a></li>
+                    			<li><a href="#">Google+</a></li>
+                    		</ul>
+                    	</div><!-- end widget -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+           	</div><!-- end container -->
+		</footer><!-- end primary-footer -->
+
+		<footer class="footer secondary-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <p>2019 &copy;The <a href="#">IslamHub</a> IslamHub INC. All rights reserved.</p>
+                    </div>
+
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <ul class="list-inline">
+                            <li>Designed: <a href="#">AW</a></li>
+                        </ul>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+		</footer><!-- end second footer -->
+	</div><!-- end wrapper -->
+
+	<!-- jQuery Files -->
+	<script src="<?=base_url()?>assets/dashboard/js/jquery.min.js"></script>
+	<script src="<?=base_url()?>assets/dashboard/js/bootstrap.min.js"></script>
+	<script src="<?=base_url()?>assets/dashboard/js/parallax.js"></script>
+	<script src="<?=base_url()?>assets/dashboard/js/animate.js"></script>
+	<script src="<?=base_url()?>assets/dashboard/js/owl.carousel.js"></script>
+	<script src="<?=base_url()?>assets/dashboard/js/custom.js"></script>
 
 </body>
 </html>
--- islamhub/application/views/halaman-awal.php
+++ islamhub/application/views/halaman-awal.php
@@ -1,97 +1,106 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<div class="container-text text-center">
-    HALAMAN AWAL
-</div>
-<div class="container">
-	<div class="row">
-	    <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
+        <section class="section transheader homepage parallax" data-stellar-background-ratio="0.5" style="background-image:url('<?=base_url()?>assets/dashboard/images/masjid.jpg');">
+            <div class="container">
+                <div class="row">   
+                    <div class="col-md-10 col-md-offset-1 col-sm-12 text-center">
+                        <h2>1001+ Masalah Hukum Islam</h2>
+                        <h2>Dikonsultasikan di IslamHub</h2>
+                        <p class="lead">Dapatkan konsultasi layanan hukum islam terpercaya dengan pakar terpercaya.</p>
+                        <form class="calculateform">
+                            <div class="item-box">
+                                <div class="item-top form-inline">
+                                    <div class="form-group">
+                                        <div class="input-group2">
+                                            <span class="input-addon">
+                                                <i class="fa fa-link"></i>
+                                            </span>
+                                            <input type="teks" class="form-control" id="teks" name="teks" placeholder="Cari pakar sekarang!">
+                                        </div>
+                                    </div>
+                                    <input type="submit" name="send" value="Cari" class="btn btn-default" />
+                                </div>
+                            </div>
+                        </form>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+        <section class="section nopad">
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="col-md-3 col-sm-6 nopad first">
+                        <div class="home-service c1">
+                            <i class="flaticon-competition"></i>
+                            <p>Forum Diskusi</p>
+                        </div><!-- end home-service -->
+                    </div><!-- end col -->
+
+                    <div class="col-md-3 col-sm-6 nopad">
+                        <div class="home-service c2">
+                            <i class="flaticon-chat"></i>
+                            <p>Chat dengan Pakar</p>
+                        </div><!-- end home-service -->
+                    </div><!-- end col -->
+
+                    <div class="col-md-3 col-sm-6 nopad">
+                        <div class="home-service c3">
+                            <i class="flaticon-domain"></i>
+                            <p>Video Hukum Islam</p>
+                        </div><!-- end home-service -->
+                    </div><!-- end col -->
+
+                    <div class="col-md-3 col-sm-6 nopad last">
+                        <div class="home-service c4">
+                            <i class="flaticon-medal"></i>
+                            <p>Buku Hukum Islam</p>
+                        </div><!-- end home-service -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+        
+        <section class="section lb nopad spotlight style1">
+            <div class="image col-md-4 hidden-sm hidden-xs">
+                <img src="<?=base_url()?>assets/dashboard/images/solat.jpg" alt="solat" />
             </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
+            <div class="content">
+                <h2>Ingin Sholat Tahajud setelah Tarawih?</h2>
+                <p>
+                    Bolehkah kita shalat tahajjud dan witir setelah shalat tarawih? Tentu saja boleh, meski telah melakukan sholat witir.
+                    Nabi ﷺ pernah menambah sholat malam dua raka’at setelah beliau berwitir. Ibunda ‘Aisyah –radhiyallahu’anha– menceritakan,
+                </p>
+                <p>
+                كَانَ يُصَلِّى ثَلاَثَ عَشْرَةَ رَكْعَةً يُصَلِّى ثَمَانَ رَكَعَاتٍ ثُمَّ يُوتِرُ ثُمَّ يُصَلِّى رَكْعَتَيْنِ وَهُوَ جَالِسٌ فَإِذَا أَرَادَ أَنْ يَرْكَعَ قَامَ فَرَكَعَ ثُمَّ يُصَلِّى رَكْعَتَيْنِ بَيْنَ النِّدَاءِ وَالإِقَامَةِ مِنْ صَلاَةِ الصُّبْحِ.
+                </p>
+                <p>
+                    “Nabi ﷺ biasa melaksanakan shalat 13 raka’at (dalam semalam). Beliau melaksanakan shalat 8 raka’at kemudian beliau berwitir 
+                    (dengan 1 raka’at). Kemudian setelah berwitir, beliau melaksanakan shalat dua raka’at sambil duduk. Jika ingin melakukan ruku’, 
+                    beliau berdiri dari ruku’nya dan beliau membungkukkan badan untuk ruku’. Setelah itu di antara waktu adzan shubuh dan iqomahnya, 
+                    beliau melakukan shalat dua raka’at.” (HR. Muslim)
+                </p>
+                 <a href="#" class="btn btn-transparent">Learn More</a>
             </div>
-        </div>		
-        <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
+        </section>
+
+        <section class="section lb nopad spotlight style1">
+            <div class="content">
+                <h2>Pahala bagi orang yang diGhibah</h2>
+                <p>
+                        Allah menyebut ghibah dalam al-Quran sebagai perbuatan makan bangkai sesama muslim.
+                        Allah berfirman,
+                </p>
+                <p>
+                        وَلَا يَغْتَبْ بَعْضُكُمْ بَعْضًا أَيُحِبُّ أَحَدُكُمْ أَنْ يَأْكُلَ لَحْمَ أَخِيهِ مَيْتًا فَكَرِهْتُمُوهُ
+                </p>
+                <p>
+                    “Janganlah menggunjingkan satu sama lain. Adakah seorang diantara kamu yang suka memakan 
+                    daging saudaranya yang sudah mati? Maka tentulah kamu merasa jijik kepadanya. (QS. al-Hujurat: 12)
+                </p>
+                 <a href="#" class="btn btn-transparent">Learn More</a>
             </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
+            <div class="image col-md-4 hidden-sm hidden-xs">
+                <img src="<?=base_url()?>assets/dashboard/images/nguping.png" alt="" />
             </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-3">
-            <div class="card">
-                <img src="img/berbagi.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-                                        <!-- Start of LiveChat (www.livechatinc.com) code -->
-<script type="text/javascript">
-window.__lc = window.__lc || {};
-window.__lc.license = 10919362;
-(function() {
-  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-})();
-</script>
-<noscript>
-<a href="https://www.livechatinc.com/chat-with/10919362/" rel="nofollow">Chat with us</a>,
-powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
-</noscript>
-<!-- End of LiveChat code -->
-                          
-        </div>
-	</div>
-</div>
-		
+        </section>
