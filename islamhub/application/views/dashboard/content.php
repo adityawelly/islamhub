a07e6839f8d496dc2b90962bd9c50376b662fd42
KimJongCal <piscalpratama@gmail.com>
2019-05-22 11:56:35 +0700

    update

--- islamhub/application/views/dashboard/content.php
+++ islamhub/application/views/dashboard/content.php
@@ -61,7 +61,7 @@
                 </div>
               </div>
               <div class="col-md-4">
-<script id="cid0020000219487547742" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 200px;height: 300px;">{"handle":"rumahkonsultasi","arch":"js","styles":{"a":"009900","b":100,"c":"FFFFFF","d":"FFFFFF","k":"009900","l":"009900","m":"009900","n":"FFFFFF","p":"10","q":"009900","r":100,"pos":"br","cv":1,"cvbg":"009900","cvw":200,"cvh":30,"ticker":1,"fwtickm":1}}</script>
+
               </div>
             </div>
             
