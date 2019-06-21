83239e5f76ad38a3c113fa34bd790ec2287d96a8
Alfi Gusman <alfigusman0@gmail.com>
2019-06-21 13:37:57 +0700

    update

--- islamhub/allvideo/application/logs/log-2019-05-22.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-05-22 04:14:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'allvideo' C:\xampp\htdocs\islamhub\islamhub\allvideo\system\database\drivers\mysqli\mysqli_driver.php 203
-ERROR - 2019-05-22 04:14:37 --> Unable to connect to the database
